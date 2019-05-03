3badbb0a9cd3e30ff60ab8fb13967cf7f5636d22
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-05-03 16:29:07 +0200

    doc(examples) Add the `simple.wasm` example.

--- examples/simple.go
+++ examples/simple.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"runtime"
+	"path"
+	"fmt"
 	"wasmer"
 )
 
 func main(){
-	wasmer.Foo()
+	_, filename, _, _ := runtime.Caller(0)
+	module_path := path.Join(path.Dir(filename), "simple.wasm")
+
+	bytes, _ := wasmer.ReadBytes(module_path)
+
+	fmt.Println(fmt.Sprintf("Is module valid? %t", wasmer.Validate(bytes)))
+
+	instance := wasmer.NewInstance(bytes)
+	result := instance.Call("sum")
+
+	fmt.Println(fmt.Sprintf("Result is: %d", result))
 }
