fe0d6f79664d405a660752da82e0fdee36a02373
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-05-10 12:56:22 +0200

    test(extension) Set up test framework, write more tests, use errors.

--- extension/src/wasmer/wasmer.go
+++ extension/src/wasmer/wasmer.go
@@ -6,6 +6,8 @@ package wasmer
 */
 import "C"
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"unsafe"
 )
@@ -18,11 +20,36 @@ func Validate(bytes []byte) bool {
 	return true == C.wasmer_validate((*C.uchar) (unsafe.Pointer(&bytes[0])), C.uint(len(bytes)))
 }
 
+type InstanceError struct {
+	message string
+}
+
+func NewInstanceError(message string) error {
+	return &InstanceError { message }
+}
+
+func (self *InstanceError) Error() string {
+	return self.message
+}
+
+type ExportedFunctionError struct {
+	function_name string
+	message string
+}
+
+func NewExportedFunctionError(function_name string, message string) error {
+	return &ExportedFunctionError { function_name, message }
+}
+
+func (self *ExportedFunctionError) Error() string {
+	return fmt.Sprintf(self.message, self.function_name)
+}
+
 type Instance struct {
 	instance *C.wasmer_instance_t
 }
 
-func NewInstance(bytes []byte) Instance {
+func NewInstance(bytes []byte) (Instance, error) {
 	var imports []C.wasmer_import_t = []C.wasmer_import_t{}
 	var instance *C.wasmer_instance_t = nil
 
@@ -34,14 +61,14 @@ func NewInstance(bytes []byte) Instance {
 		C.int(0),
 	)
 
-	if (C.WASMER_OK != compile_result) {
-		panic("Failed to compile the module.")
+	if C.WASMER_OK != compile_result {
+		return Instance { instance: nil }, NewInstanceError("Failed to compile the module.")
 	}
 
-	return Instance { instance }
+	return Instance { instance }, nil
 }
 
-func (self Instance) Call(function_name string, arguments []Value) Value {
+func (self Instance) Call(function_name string, arguments []Value) (Value, error) {
 	var wasm_arguments []C.wasmer_value_t = make([]C.wasmer_value_t, len(arguments))
 
 	for index, value := range arguments {
@@ -69,31 +96,48 @@ func (self Instance) Call(function_name string, arguments []Value) Value {
 	var wasm_function_name = C.CString(function_name)
 	defer C.free(unsafe.Pointer(wasm_function_name))
 
+	var wasm_argument_c_pointer *C.wasmer_value_t = nil
+
+	if len(arguments) > 0 {
+		wasm_argument_c_pointer = (*C.wasmer_value_t) (unsafe.Pointer(&wasm_arguments[0]))
+	}
+
 	var call_result C.wasmer_result_t = C.wasmer_instance_call(
 		self.instance,
 		wasm_function_name,
-		(*C.wasmer_value_t) (unsafe.Pointer(&wasm_arguments[0])),
+		wasm_argument_c_pointer,
 		C.int(len(wasm_arguments)),
 		(*C.wasmer_value_t) (unsafe.Pointer(&wasm_results[0])),
 		C.int(len(wasm_results)),
 	)
 
 	if (C.WASMER_OK != call_result) {
-		panic("Failed to call the `sum` function.")
+		return ValueI32(0), NewExportedFunctionError(function_name, "Failed to call the `%s` exported function.")
 	}
 
 	var result = wasm_results[0];
 
 	switch result.tag {
 	case C.WASM_I32:
-		return ValueI32(int32(result.value[C.WASM_I32]));
+		return ValueI32(int32(result.value[C.WASM_I32])), nil
 	case C.WASM_I64:
-		return ValueI64(int64(result.value[C.WASM_I64]));
+		return ValueI64(int64(result.value[C.WASM_I64])), nil
 	case C.WASM_F32:
-		return ValueF32(float32(result.value[C.WASM_F32]));
+		return ValueF32(float32(result.value[C.WASM_F32])), nil
 	case C.WASM_F64:
-		return ValueF64(float64(result.value[C.WASM_F64]));
+		return ValueF64(float64(result.value[C.WASM_F64])), nil
 	default:
 		panic("unreachable")
 	}
+}
+
+func GetLastError() (string, error) {
+	var error_length C.int = C.wasmer_last_error_length()
+	var error_message *C.char = nil
+
+	if -1 == C.wasmer_last_error_message(error_message, error_length) {
+		return "", errors.New("Cannot read last error.")
+	}
+
+	return C.GoStringN(error_message, error_length), nil
 }
--- extension/src/wasmer/wasmer_test.go
+++ extension/src/wasmer/wasmer_test.go
@@ -4,6 +4,7 @@ import (
 	"path"
 	"runtime"
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func GetBytes() []byte {
@@ -15,18 +16,70 @@ func GetBytes() []byte {
 	return bytes
 }
 
+func GetInvalidBytes() []byte {
+	_, filename, _, _ := runtime.Caller(0)
+	module_path := path.Join(path.Dir(filename), "..", "..", "tests", "invalid.wasm")
+
+	bytes, _ := ReadBytes(module_path)
+
+	return bytes
+}
+
 func TestValidate(t *testing.T) {
 	output := Validate(GetBytes())
 
-	if !output {
-		t.Error("Failed to validate the module.")
-	}
+	assert.True(t, output)
 }
 
 func TestValidateInvalid(t *testing.T) {
-	output := Validate([]byte{0, 1, 2, 3})
+	output := Validate(GetInvalidBytes())
 
-	if output {
-		t.Error("Failed to invalidate the supposed module.")
-	}
+	assert.False(t, output)
+}
+
+func TestInstantiate(t *testing.T) {
+	instance, err := NewInstance(GetBytes())
+
+	assert.NotNil(t, instance.instance)
+	assert.NoError(t, err)
+}
+
+func TestInstantiateInvalidModule(t *testing.T) {
+	instance, err := NewInstance(GetInvalidBytes())
+
+	assert.Error(t, err, "Failed to compile the module.")
+	assert.Nil(t, instance.instance)
+}
+
+func TestBasicSum(t *testing.T) {
+	instance, _ := NewInstance(GetBytes())
+	output, err := instance.Call("sum", []Value{ValueI32(1), ValueI32(2)})
+
+	assert.Equal(t, ValueI32(3), output)
+	assert.NoError(t, err)
+}
+
+func TestCallUndefinedFunction(t *testing.T) {
+	instance, _ := NewInstance(GetBytes())
+	function_name := "foo"
+	output, err := instance.Call(function_name, []Value{})
+
+	assert.Equal(t, ValueI32(0), output)
+	assert.Errorf(t, err, "Failed to call the `%s` exported function", function_name)
+}
+
+func TestCallI32I32(t *testing.T) {
+	instance, _ := NewInstance(GetBytes())
+	output, err := instance.Call("i32_i32", []Value{ValueI32(7)})
+
+	assert.Equal(t, ValueI32(7), output)
+	assert.NoError(t, err)
+}
+
+func TestCallI64I64(t *testing.T) {
+	instance, _ := NewInstance(GetBytes())
+	output, err := instance.Call("i64_i64", []Value{ValueI64(7)})
+
+	assert.Equal(t, ValueI64(7), output)
+	assert.NoError(t, err)
 }
