7a2200dec20226663063f17f108df5925e7ea965
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-05-14 15:23:29 +0200

    chore(extension) Extract the `error.go` file.

---
+++ extension/src/wasmer/error.go
@@ -0,0 +1,30 @@
+package wasmer
+
+/*
+#cgo LDFLAGS: -L../../ -lwasmer_runtime_c_api
+#include "../../wasmer.h"
+*/
+import "C"
+import (
+	"errors"
+	"unsafe"
+)
+
+func GetLastError() (string, error) {
+	var error_length C.int = C.wasmer_last_error_length()
+
+	if error_length == 0 {
+		return "", nil
+	}
+
+	var error_message []C.char = make([]C.char, error_length)
+	var error_message_pointer *C.char = (*C.char) (unsafe.Pointer(&error_message[0]))
+
+	var error_result = C.wasmer_last_error_message(error_message_pointer, error_length)
+
+	if -1 == error_result {
+		return "", errors.New("Cannot read last error.")
+	}
+
+	return C.GoString(error_message_pointer), nil
+}
--- extension/src/wasmer/instance.go
+++ extension/src/wasmer/instance.go
@@ -6,7 +6,6 @@ package wasmer
 */
 import "C"
 import (
-	"errors"
 	"fmt"
 	"unsafe"
 )
@@ -205,23 +204,4 @@ func (self Instance) Call(function_name string, arguments ...Value) (Value, erro
 	} else {
 		return Void(), nil
 	}
-}
-
-func GetLastError() (string, error) {
-	var error_length C.int = C.wasmer_last_error_length()
-
-	if error_length == 0 {
-		return "", nil
-	}
-
-	var error_message []C.char = make([]C.char, error_length)
-	var error_message_pointer *C.char = (*C.char) (unsafe.Pointer(&error_message[0]))
-
-	var error_result = C.wasmer_last_error_message(error_message_pointer, error_length)
-
-	if -1 == error_result {
-		return "", errors.New("Cannot read last error.")
-	}
-
-	return C.GoString(error_message_pointer), nil
 }
