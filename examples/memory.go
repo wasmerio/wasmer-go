e0acff1daa208a7f9fec2a0286fccb46f6c979a9
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-05-15 12:08:59 +0200

    examples(memory) Remove comments.

--- examples/memory.go
+++ examples/memory.go
@@ -27,26 +27,3 @@ func main() {
 	fmt.Println(string(memory[pointer : pointer+13]))
 
 }
-
-/*
-<?php
-
-declare(strict_types = 1);
-
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-$instance = new Wasm\Instance(__DIR__ . '/memory.wasm');
-$pointer = $instance->return_hello();
-
-$memory = new Wasm\Uint8Array($instance->getMemoryBuffer(), $pointer);
-
-$nth = 0;
-
-while (0 !== $memory[$nth]) {
-    echo chr($memory[$nth]);
-    ++$nth;
-}
-
-echo "\n";
-
-*/
