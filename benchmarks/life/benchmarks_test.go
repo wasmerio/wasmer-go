d75c681f1b6b274931ec49c99dd50d8244fa81ab
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-05-28 11:35:33 +0200

    test(benchmarks) Improve the `life` benchmark suite.

--- benchmarks/life/benchmarks_test.go
+++ benchmarks/life/benchmarks_test.go
@@ -2,7 +2,7 @@ package wasmer_test
 
 import (
 	life "github.com/perlin-network/life/exec"
-	"github.com/wasmerio/go-ext-wasm/wasmer"
+	"io/ioutil"
 	"path"
 	"runtime"
 	"testing"
@@ -10,33 +10,38 @@ import (
 
 const N = 100000
 
-func GetBytes() []byte {
+func GetBytes(wasmFile string) []byte {
 	_, filename, _, _ := runtime.Caller(0)
-	modulePath := path.Join(path.Dir(filename), "testdata", "benchmarks", "nbody.wasm")
+	modulePath := path.Join(path.Dir(filename), "testdata", "benchmarks", wasmFile)
 
-	bytes, _ := wasmer.ReadBytes(modulePath)
+	bytes, _ := ioutil.ReadFile(modulePath)
 
 	return bytes
 }
 
-func BenchmarkWasmer(b *testing.B) {
-	wasmBytes := GetBytes()
-
-	for i := 0; i < b.N; i++ {
-		instance, _ := wasmer.NewInstance(wasmBytes)
-		result, _ := instance.Exports["main"](N)
-
-		_ = result
-	}
+func Benchmark_Life_Nbody(b *testing.B) {
+	benchmarkLife(b, "nbody.wasm", "main", N)
 }
 
-func BenchmarkLife(b *testing.B) {
-	wasmBytes := GetBytes()
+func Benchmark_Life_Fibonacci_Recursive(b *testing.B) {
+	benchmarkLife(b, "fib_recursive.wasm", "app_main")
+}
+
+func Benchmark_Life_Pollard_Rho_128(b *testing.B) {
+	benchmarkLife(b, "pollard_rho_128.wasm", "app_main")
+}
+
+func Benchmark_Life_Snappy_Compress(b *testing.B) {
+	benchmarkLife(b, "snappy_compress.wasm", "app_main")
+}
+
+func benchmarkLife(b *testing.B, wasmFile string, exportName string, exportValues ...int64) {
+	wasmBytes := GetBytes(wasmFile)
 
 	for i := 0; i < b.N; i++ {
 		vm, _ := life.NewVirtualMachine(wasmBytes, life.VMConfig{}, &life.NopResolver{}, nil)
-		entryID, _ := vm.GetFunctionExport("main")
-		result, _ := vm.Run(entryID, N)
+		entryID, _ := vm.GetFunctionExport(exportName)
+		result, _ := vm.Run(entryID, exportValues...)
 
 		_ = result
 	}
--- benchmarks/life/go.mod
+++ benchmarks/life/go.mod
@@ -5,7 +5,9 @@ go 1.12
 replace github.com/go-interpreter/wagon v0.0.0 => github.com/perlin-network/wagon v0.3.1-0.20180825141017-f8cb99b55a39
 
 require (
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/perlin-network/life v0.0.0-20190521143330-57f3819c2df0
-	github.com/wasmerio/go-ext-wasm v0.0.0-20190527154049-20a5840a9e4f
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58 // indirect
 	google.golang.org/appengine v1.6.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
