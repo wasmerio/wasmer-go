b11aa72d003509b0354ffa90a22870e6072be461
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-07-08 12:00:17 +0200

    feat(memory) Add the `Memory.Grow` method.
    This method can be used to grow the memory by a number of pages (65kb each).

--- wasmer/memory.go
+++ wasmer/memory.go
@@ -1,9 +1,28 @@
 package wasmer
 
 import (
+	"fmt"
 	"reflect"
 	"unsafe"
 )
+
+// MemoryError represents any kind of errors related to a WebAssembly memory. It
+// is returned by `Memory` functions only.
+type MemoryError struct {
+	// Error message.
+	message string
+}
+
+// NewMemoryError constructs a new `MemoryError`.
+func NewMemoryError(message string) *MemoryError {
+	return &MemoryError{message}
+}
+
+// `MemoryError` is an actual error. The `Error` function returns
+// the error message.
+func (error *MemoryError) Error() string {
+	return error.message
+}
 
 // Memory represents an exported memory of a WebAssembly instance. To read
 // and write data, please see the `Data` function.
@@ -34,4 +53,24 @@ func (memory *Memory) Data() []byte {
 	header.Cap = int(length)
 
 	return *(*[]byte)(unsafe.Pointer(&header))
+}
+
+// Grow the memory by a number of pages (65kb each).
+func (memory *Memory) Grow(numberOfPages uint32) error {
+	var growResult = cWasmerMemoryGrow(memory.memory, cUint32T(numberOfPages))
+
+	if growResult != cWasmerOk {
+		var lastError, err = GetLastError()
+		var errorMessage = "Failed to grow the memory:\n    %s"
+
+		if err != nil {
+			errorMessage = fmt.Sprintf(errorMessage, "(unknown details)")
+		} else {
+			errorMessage = fmt.Sprintf(errorMessage, lastError)
+		}
+
+		return NewMemoryError(errorMessage)
+	}
+
+	return nil
 }
