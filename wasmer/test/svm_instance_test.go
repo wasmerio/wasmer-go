7dda48bb881164059cbd609c9e9e350f512a7b76
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-08-06 13:02:51 +0300

    svm_instance_test.go - `TestNewSvmInstanceNoImports` test works

--- wasmer/test/svm_instance_test.go
+++ wasmer/test/svm_instance_test.go
@@ -10,17 +10,15 @@ import (
 type DummyNodeData struct {
 }
 
-func TestNewSvmInstance(t *testing.T) {
-	bytes := getImportedFunctionBytes("examples", "greet.wasm")
-	// bytes := getImportedFunctionBytes("examples", "imported_function.wasm")
+func TestNewSvmInstanceNoImports(t *testing.T) {
+	bytes := getImportedFunctionBytes("examples", "simple.wasm")
 
 	module, err := wasm.Compile(bytes)
 	assert.NoError(t, err)
+	defer module.Close()
 
-	addr := [32]byte{}
-	addr[0] = 10
-	addr[1] = 20
-	addr[2] = 30
+	addr := [32]byte {0}
+	addr[0], addr[1], addr[2] = 10, 20, 30
 
 	maxPages := uint(5)
 	maxPagesSlices := uint(100)
@@ -28,6 +26,12 @@ func TestNewSvmInstance(t *testing.T) {
 	config := wasm.NewSvmInstanceConfig(addr[:], maxPages, maxPagesSlices, nodeDataPtr)
 	imports := wasm.NewImports()
 
-	// wasm.NewInstanceFromModule(&module, imports)
-	wasm.NewSvmInstance(&module, imports, config)
+	instance, err := wasm.NewSvmInstance(&module, imports, config)
+	assert.NoError(t, err)
+	defer instance.Close()
+
+	sum := instance.Exports["sum"]
+	output, err := sum(10, 20)
+	assert.Equal(t, wasm.I32(30), output)
+	assert.NoError(t, err)
 }
