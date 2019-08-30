872c9a829d152647ebbe0896ddf4c3650343f641
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-08-30 17:38:06 +0200

    Added (failing) test for exceeding gas... it need more than 1 instruction to check it

--- wasmer/test/module_test.go
+++ wasmer/test/module_test.go
@@ -102,6 +102,31 @@ func TestModuleRunWithLimit(t *testing.T) {
 	assert.Equal(t, points, uint64(127))
 }
 
+// TODO: this only passes if the metering feature flag is enabled in the rust binary
+func TestModuleRunExceedsLimit(t *testing.T) {
+	module, err := wasm.CompileWithLimit(GetBytes(), 5)
+	defer module.Close()
+	assert.NoError(t, err)
+
+	instance, err := module.Instantiate()
+	defer instance.Close()
+	assert.NoError(t, err)
+
+	// we start at 0
+	points := instance.GetPointsUsed()
+	assert.Equal(t, points, uint64(0))
+	// but let's hit the limit
+	instance.SetPointsUsed(1001)
+
+	// this should be out-of-gas
+	_, err = instance.Exports["sum"](1, 2)
+	assert.Error(t, err)
+
+	// and 4 points are added anyway :(
+	points = instance.GetPointsUsed()
+	assert.Equal(t, points, uint64(1005))
+}
+
 func TestModuleSerialize(t *testing.T) {
 	module1, err := wasm.Compile(GetBytes())
 	defer module1.Close()
