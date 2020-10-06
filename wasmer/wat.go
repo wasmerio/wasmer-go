0173b4af2eb7fdd2cf6d0b603bed5acf8b7def22
Ivan Enderlin <ivan@mnt.io>
2020-10-06 21:03:42 +0200

    feat: Update `Wat2Wasm` since changes in `libwasmer_c_api`.

--- wasmer/wat.go
+++ wasmer/wat.go
@@ -2,12 +2,12 @@ package wasmer
 
 // #include <wasmer_wasm.h>
 //
-// void to_wat2wasm(uint8_t *wat_data, size_t wat_length, wasm_byte_vec_t *wasm) {
+// wasm_byte_vec_t *to_wat2wasm(uint8_t *wat_data, size_t wat_length) {
 //     wasm_byte_vec_t wat;
 //     wat.size = wat_length;
 //     wat.data = (wasm_byte_t*) wat_data;
 //
-//     wat2wasm(&wat, wasm);
+//     return wat2wasm(&wat);
 // }
 import "C"
 import (
@@ -24,17 +24,15 @@ func Wat2Wasm(wat string) ([]byte, error) {
 		watPtr = (*C.uint8_t)(unsafe.Pointer(&watAsBytes[0]))
 	}
 
-	wasm := C.wasm_byte_vec_t{}
-
-	C.to_wat2wasm(watPtr, C.size_t(watLength), &wasm)
+	var wasm *C.wasm_byte_vec_t = C.to_wat2wasm(watPtr, C.size_t(watLength))
 	runtime.KeepAlive(wat)
 
-	if wasm.data == nil {
+	if wasm == nil {
 		return nil, newErrorFromWasmer()
 	}
 
 	wasmBytes := C.GoBytes(unsafe.Pointer(wasm.data), C.int(wasm.size))
-	C.wasm_byte_vec_delete(&wasm)
+	C.wasm_byte_vec_delete(wasm)
 
 	return wasmBytes, nil
 }
