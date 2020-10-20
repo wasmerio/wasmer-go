dd08312cb84a0475e6a4faae302b01ec894a58e6
Ivan Enderlin <ivan@mnt.io>
2020-10-20 15:37:03 +0200

    test: Test `Memory.Data()`.

--- wasmer/memory_test.go
+++ wasmer/memory_test.go
@@ -43,3 +43,50 @@ func TestMemorySize(t *testing.T) {
 
 	assert.Equal(t, size, Pages(1))
 }
+
+func TestMemoryDataSize(t *testing.T) {
+	engine := NewEngine()
+	store := NewStore(engine)
+	limits, err := NewLimits(1, 7)
+
+	assert.NoError(t, err)
+
+	memory := NewMemory(store, NewMemoryType(limits))
+	dataSize := memory.DataSize()
+
+	assert.Equal(t, dataSize, uint(0x10000))
+}
+
+func TestMemoryData(t *testing.T) {
+	instance := testGetInstance(t)
+
+	getString, err := instance.Exports.GetFunction("string")
+
+	assert.NoError(t, err)
+
+	ptr, err := getString()
+
+	assert.NoError(t, err)
+	assert.Equal(t, ptr, int32(1048576))
+
+	pointer := ptr.(int32)
+
+	memory1, err := instance.Exports.GetMemory("memory")
+
+	assert.NoError(t, err)
+
+	memory2, err := instance.Exports.GetMemory("memory")
+
+	data1 := memory1.Data()
+	data2 := memory2.Data()
+
+	assert.NoError(t, err)
+	assert.Equal(t, "Hello, World!", string(data1[pointer:pointer+13]))
+	assert.Equal(t, "Hello, World!", string(data2[pointer:pointer+13]))
+
+	data1[pointer] = 'A'
+
+	assert.Equal(t, "Aello, World!", string(data1[pointer:pointer+13]))
+	assert.Equal(t, "Aello, World!", string(data2[pointer:pointer+13]))
+
+}
