2913691567b5612930f372770973c409b4b46f9e
Ivan Enderlin <ivan@mnt.io>
2020-10-20 15:02:05 +0200

    feat: Implement `NewMemory` and `Memory.Type()` + `Memory.Size()`.

--- wasmer/memory.go
+++ wasmer/memory.go
@@ -21,6 +21,14 @@ func newMemory(pointer *C.wasm_memory_t, ownedBy interface{}) *Memory {
 	return memory
 }
 
+func NewMemory(store *Store, ty *MemoryType) *Memory {
+	pointer := C.wasm_memory_new(store.inner(), ty.inner())
+
+	runtime.KeepAlive(store)
+
+	return newMemory(pointer, nil)
+}
+
 func (self *Memory) inner() *C.wasm_memory_t {
 	return self._inner
 }
@@ -31,6 +39,18 @@ func (self *Memory) ownedBy() interface{} {
 	}
 
 	return self._ownedBy
+}
+
+func (self *Memory) Type() *MemoryType {
+	ty := C.wasm_memory_type(self.inner())
+
+	runtime.KeepAlive(self)
+
+	return newMemoryType(ty, self.ownedBy())
+}
+
+func (self *Memory) Size() Pages {
+	return Pages(C.wasm_memory_size(self.inner()))
 }
 
 func (self *Memory) IntoExtern() *Extern {
