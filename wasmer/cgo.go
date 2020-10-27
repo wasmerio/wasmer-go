ca73b6b7f5269e246512966df4647895a3f99f91
Ivan Enderlin <ivan@mnt.io>
2020-10-27 15:13:22 +0100

    chore: Reorganize the `include/` directory.

--- wasmer/cgo.go
+++ wasmer/cgo.go
@@ -1,5 +1,10 @@
 package wasmer
 
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR}/include/ -L${SRCDIR}/include/ -lwasmer_c_api
+// #cgo CFLAGS: -I${SRCDIR}/packaged/include
+// #cgo LDFLAGS: -lwasmer
+//
+// #cgo linux,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/linux-amd64 -L${SRCDIR}/packaged/lib/linux-amd64
+// #cgo darwin,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/darwin-amd64 -L${SRCDIR}/packaged/lib/darwin-amd64
+//
 // #include <wasmer_wasm.h>
 import "C"
