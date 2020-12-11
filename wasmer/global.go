1eff4a9734113c245263d64d4bcfa8ab53b4fe65
jubianchi <julien@wasmer.io>
2020-12-11 15:09:49 +0100

    doc: Document Global public API

--- wasmer/global.go
+++ wasmer/global.go
@@ -23,6 +23,14 @@ func newGlobal(pointer *C.wasm_global_t, ownedBy interface{}) *Global {
 	return global
 }
 
+// NewGlobal instantiates a new Global in the given Store.
+//
+// It takes three arguments, the Store, the GlobalType and the Value for the Global.
+//
+//   valueType := NewValueType(I32)
+//   globalType := NewGlobalType(valueType, CONST)
+//   global := NewGlobal(store, globalType, NewValue(42, I32))
+//
 func NewGlobal(store *Store, ty *GlobalType, value Value) *Global {
 	pointer := C.wasm_global_new(
 		store.inner(),
@@ -45,12 +53,22 @@ func (self *Global) ownedBy() interface{} {
 	return self._ownedBy
 }
 
+// IntoExtern converts the Global into an Extern.
+//
+//   global, _ := instance.Exports.GetGlobal("exported_global")
+//   extern := global.IntoExtern()
+//
 func (self *Global) IntoExtern() *Extern {
 	pointer := C.wasm_global_as_extern(self.inner())
 
 	return newExtern(pointer, self.ownedBy())
 }
 
+// Type returns the Global's GlobalType.
+//
+//   global, _ := instance.Exports.GetGlobal("exported_global")
+//   ty := global.Type()
+//
 func (self *Global) Type() *GlobalType {
 	ty := C.wasm_global_type(self.inner())
 
@@ -59,6 +77,13 @@ func (self *Global) Type() *GlobalType {
 	return newGlobalType(ty, self.ownedBy())
 }
 
+// Set sets the Global's value.
+//
+// It takes two arguments, the Global's value as a native Go value and the value's ValueKind.
+//
+//   global, _ := instance.Exports.GetGlobal("exported_global")
+//   _ = global.Set(1, I32)
+//
 func (self *Global) Set(value interface{}, kind ValueKind) error {
 	if self.Type().Mutability() == IMMUTABLE {
 		return newErrorWith("The global variable is not mutable, cannot set a new value")
@@ -76,6 +101,11 @@ func (self *Global) Set(value interface{}, kind ValueKind) error {
 	return nil
 }
 
+// Get returns the Global's value as a native Go value.
+//
+//   global, _ := instance.Exports.GetGlobal("exported_global")
+//   value, _ := global.Get()
+//
 func (self *Global) Get() (interface{}, error) {
 	var value C.wasm_val_t
 
