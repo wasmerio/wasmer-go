48afb1a52e56c2ac0f66c5843294afd4cdd0163e
jubianchi <julien@wasmer.io>
2020-12-11 15:08:49 +0100

    doc: Document Exports public API

--- wasmer/exports.go
+++ wasmer/exports.go
@@ -45,6 +45,13 @@ func (self *Exports) inner() *C.wasm_extern_vec_t {
 	return &self._inner
 }
 
+// Get retrieves and returns an Extern by its name.
+//
+// ❗️ If the name does not refer to an existing export, Get will return an Error.
+//
+//   instance, _ := NewInstance(module, NewImportObject())
+//   extern, error := instance.Exports.Get("an_export")
+//
 func (self *Exports) Get(name string) (*Extern, error) {
 	export, exists := self.exports[name]
 
@@ -55,6 +62,19 @@ func (self *Exports) Get(name string) (*Extern, error) {
 	return export, nil
 }
 
+// GetRawFunction retrieves and returns a exported Function by its name.
+//
+// ❗️ If the name does not refer to an existing export, GetRawFunction will return an Error.
+//
+// ⚠️ If the export is not a function, GetRawFunction will return nil as its result.
+//
+//   instance, _ := NewInstance(module, NewImportObject())
+//   exportedFunc, error := instance.Exports.GetRawFunction("an_exported_function")
+//
+//   if error != nil && exportedFunc != nil {
+//       exportedFunc.Call()
+//   }
+//
 func (self *Exports) GetRawFunction(name string) (*Function, error) {
 	exports, err := self.Get(name)
 
@@ -65,6 +85,19 @@ func (self *Exports) GetRawFunction(name string) (*Function, error) {
 	return exports.IntoFunction(), nil
 }
 
+// GetFunction retrieves a exported function by its name and returns it as a native Go function.
+//
+// ❗️ If the name does not refer to an existing export, GetFunction will return an Error.
+//
+// ⚠️ If the export is not a function, GetFunction will return nil as its result.
+//
+//   instance, _ := NewInstance(module, NewImportObject())
+//   exportedFunc, error := instance.Exports.GetFunction("an_exported_function")
+//
+//   if error != nil && exportedFunc != nil {
+//       exportedFunc()
+//   }
+//
 func (self *Exports) GetFunction(name string) (func(...interface{}) (interface{}, error), error) {
 	function, err := self.GetRawFunction(name)
 
@@ -75,6 +108,15 @@ func (self *Exports) GetFunction(name string) (func(...interface{}) (interface{}
 	return function.Native(), nil
 }
 
+// GetGlobal retrieves and returns a exported Global by its name.
+//
+// ❗️ If the name does not refer to an existing export, GetGlobal will return an Error.
+//
+// ⚠️ If the export is not a global, GetGlobal will return nil as a result.
+//
+//   instance, _ := NewInstance(module, NewImportObject())
+//   exportedGlobal, error := instance.Exports.GetGlobal("an_exported_global")
+//
 func (self *Exports) GetGlobal(name string) (*Global, error) {
 	exports, err := self.Get(name)
 
@@ -85,6 +127,15 @@ func (self *Exports) GetGlobal(name string) (*Global, error) {
 	return exports.IntoGlobal(), nil
 }
 
+// GetTable retrieves and returns a exported Table by its name.
+//
+// ❗️ If the name does not refer to an existing export, GetTable will return an Error.
+//
+// ⚠️ If the export is not a table, GetTable will return nil as a result.
+//
+//   instance, _ := NewInstance(module, NewImportObject())
+//   exportedTable, error := instance.Exports.GetTable("an_exported_table")
+//
 func (self *Exports) GetTable(name string) (*Table, error) {
 	exports, err := self.Get(name)
 
@@ -95,6 +146,15 @@ func (self *Exports) GetTable(name string) (*Table, error) {
 	return exports.IntoTable(), nil
 }
 
+// GetMemory retrieves and returns a exported Memory by its name.
+//
+// ❗️ If the name does not refer to an existing export, GetMemory will return an Error.
+//
+// ⚠️ If the export is not a memory, GetMemory will return nil as a result.
+//
+//   instance, _ := NewInstance(module, NewImportObject())
+//   exportedMemory, error := instance.Exports.GetMemory("an_exported_memory")
+//
 func (self *Exports) GetMemory(name string) (*Memory, error) {
 	exports, err := self.Get(name)
 
