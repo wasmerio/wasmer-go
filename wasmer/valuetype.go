16e8ae57409d8843b7106e14b22fc02c093e59ac
jubianchi <julien@wasmer.io>
2020-12-15 16:39:16 +0100

    doc: Document ValueType public API

--- wasmer/valuetype.go
+++ wasmer/valuetype.go
@@ -18,6 +18,15 @@ const (
 	FuncRef = ValueKind(C.WASM_FUNCREF)
 )
 
+// String returns the ValueKind as a string.
+//
+//   I32.String()     // "i32"
+//   I64.String()     // "i64"
+//   F32.String()     // "f32"
+//   F64.String()     // "f64"
+//   AnyRef.String()  // "anyref"
+//   FuncRef.String() // "funcref"
+//
 func (self ValueKind) String() string {
 	switch self {
 	case I32:
@@ -33,13 +42,31 @@ func (self ValueKind) String() string {
 	case FuncRef:
 		return "funcref"
 	}
-	panic("Unknown value kind") // unreachable
+	panic("Unknown value kind")
 }
 
+// IsNumber returns true if the ValueKind is a number type.
+//
+//   I32.IsNumber()     // true
+//   I64.IsNumber()     // true
+//   F32.IsNumber()     // true
+//   F64.IsNumber()     // true
+//   AnyRef.IsNumber()  // false
+//   FuncRef.IsNumber() // false
+//
 func (self ValueKind) IsNumber() bool {
 	return bool(C.wasm_valkind_is_num(C.wasm_valkind_t(self)))
 }
 
+// IsReference returns true if the ValueKind is a reference.
+//
+//   I32.IsReference()     // false
+//   I64.IsReference()     // false
+//   F32.IsReference()     // false
+//   F64.IsReference()     // false
+//   AnyRef.IsReference()  // true
+//   FuncRef.IsReference() // true
+//
 func (self ValueKind) IsReference() bool {
 	return bool(C.wasm_valkind_is_ref(C.wasm_valkind_t(self)))
 }
@@ -48,11 +75,18 @@ func (self ValueKind) inner() C.wasm_valkind_t {
 	return C.wasm_valkind_t(self)
 }
 
+// ValueType classifies the individual values that WebAssembly code can compute with and the values that a variable
+// accepts.
+//
 type ValueType struct {
 	_inner   *C.wasm_valtype_t
 	_ownedBy interface{}
 }
 
+// NewValueType instantiates a new ValueType given a ValueKind.
+//
+//   valueType := NewValueType(I32)
+//
 func NewValueType(kind ValueKind) *ValueType {
 	pointer := C.wasm_valtype_new(C.wasm_valkind_t(kind))
 
@@ -75,6 +109,11 @@ func (self *ValueType) inner() *C.wasm_valtype_t {
 	return self._inner
 }
 
+// Kind returns the ValueType's ValueKind
+//
+//   valueType := NewValueType(I32)
+//   _ = valueType.Kind()
+//
 func (self *ValueType) Kind() ValueKind {
 	kind := ValueKind(C.wasm_valtype_kind(self.inner()))
 
@@ -83,6 +122,15 @@ func (self *ValueType) Kind() ValueKind {
 	return kind
 }
 
+// NewValueTypes instantiates a new ValueType array from a list of ValueKind. Not that the list
+// may be empty.
+//
+//   valueTypes := NewValueTypes(I32, I64, F32)
+//
+// ℹ️ NewValueTypes is specifically designed to help you declare function types:
+//
+//   functionType := NewFunctionType(NewValueTypes(), NewValueTypes(I32))
+//
 func NewValueTypes(kinds ...ValueKind) []*ValueType {
 	valueTypes := make([]*ValueType, len(kinds))
 
