829b7ddf9f127c63d166e1b5f10e0b44e0a16912
jubianchi <julien@wasmer.io>
2020-12-15 15:41:28 +0100

    doc: Document Store public API

--- wasmer/store.go
+++ wasmer/store.go
@@ -4,21 +4,27 @@ package wasmer
 import "C"
 import "runtime"
 
-// Store represents all global state that can be manipulated by
-// WebAssembly programs. It consists of the runtime representation
-// of all instances of functions, tables, memories, and globals that
-// have been allocated during the lifetime of the abstract machine.
+// Store represents all global state that can be manipulated by WebAssembly programs. It consists of the runtime
+// representation of all instances of functions, tables, memories, and globals that have been allocated during the life
+// time of the abstract machine.
 //
-// The `Store` holds the engine (that is —amongst many things— used to compile
-// the Wasm bytes into a valid module artifact), in addition to the
-// [`Tunables`] (that are used to create the memories, tables and globals).
+// The Store holds the Engine (that is — amongst many things — used to compile
+// the Wasm bytes into a valid module artifact).
 //
-// Spec: https://webassembly.github.io/spec/core/exec/runtime.html#store
+// See also
+//
+// Specification: https://webassembly.github.io/spec/core/exec/runtime.html#store
+//
 type Store struct {
 	_inner *C.wasm_store_t
 	Engine *Engine
 }
 
+// NewStore instantiates a new Store with an Engine.
+//
+//   engine := NewEngine()
+//   store := NewStore(engine)
+//
 func NewStore(engine *Engine) *Store {
 	self := &Store{
 		_inner: C.wasm_store_new(engine.inner()),
