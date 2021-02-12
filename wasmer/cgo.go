308f8d968cd9c3f2760e6fa0b8304bdc791d95e5
Ivan Enderlin <ivan@mnt.io>
2021-02-12 13:29:58 +0100

    feat(cgo) Remove `-rpath`.

--- wasmer/cgo.go
+++ wasmer/cgo.go
@@ -5,10 +5,10 @@ package wasmer
 // #cgo CFLAGS: -I${SRCDIR}/packaged/include
 // #cgo LDFLAGS: -lwasmer
 //
-// #cgo darwin,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/darwin-amd64 -L${SRCDIR}/packaged/lib/darwin-amd64
-// #cgo linux,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/linux-amd64 -L${SRCDIR}/packaged/lib/linux-amd64
-// #cgo linux,arm64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/linux-aarch64 -L${SRCDIR}/packaged/lib/linux-aarch64
-// #cgo windows,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/windows-amd64 -L${SRCDIR}/packaged/lib/windows-amd64
+// #cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/packaged/lib/darwin-amd64
+// #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/packaged/lib/linux-amd64
+// #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/packaged/lib/linux-aarch64
+// #cgo windows,amd64 LDFLAGS: -L${SRCDIR}/packaged/lib/windows-amd64
 //
 // #include <wasmer_wasm.h>
 import "C"
