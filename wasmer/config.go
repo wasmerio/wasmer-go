f119ff47bc732755ee497e1b5fd92c683e916bf6
Ivan Enderlin <ivan@mnt.io>
2021-02-18 21:41:42 +0100

    feat(config) Add the `CompilerKind` and `EngineKind` types.

--- wasmer/config.go
+++ wasmer/config.go
@@ -3,6 +3,80 @@ package wasmer
 // #include <wasmer_wasm.h>
 import "C"
 
+// CompilerKind represents the possible compiler types.
+type CompilerKind C.wasmer_compiler_t
+
+const (
+	// Represents the Cranelift compiler.
+	CRANELIFT = CompilerKind(C.CRANELIFT)
+
+	// Represents the LLVM compiler.
+	LLVM = CompilerKind(C.LLVM)
+
+	// Represents the Singlepass compiler.
+	SINGLEPASS = CompilerKind(C.SINGLEPASS)
+)
+
+// Strings returns the CompilerKind as a string.
+//
+//   CRANELIFT.String() // "cranelift"
+//   LLVM.String() // "llvm"
+func (self CompilerKind) String() string {
+	switch self {
+	case CRANELIFT:
+		return "cranelift"
+
+	case LLVM:
+		return "llvm"
+
+	case SINGLEPASS:
+		return "singlepass"
+	}
+	panic("Unknown compiler")
+}
+
+// IsCompilerAvailable checks that the given compiler is available
+// with the current `wasmer-go` built.
+//
+//   IsCompilerAvailable(CRANELIFT)
+func IsCompilerAvailable(compiler CompilerKind) bool {
+	return bool(C.wasmer_is_compiler_available(C.wasmer_compiler_t(compiler)))
+}
+
+// EngineKind represents the possible engine types.
+type EngineKind C.wasmer_engine_t
+
+const (
+	// Represents the JIT engine.
+	JIT = EngineKind(C.JIT)
+
+	// Represents the Native engine.
+	NATIVE = EngineKind(C.NATIVE)
+)
+
+// Strings returns the EngineKind as a string.
+//
+//   JIT.String() // "jit"
+//   NATIVE.String() // "native"
+func (self EngineKind) String() string {
+	switch self {
+	case JIT:
+		return "jit"
+
+	case NATIVE:
+		return "native"
+	}
+	panic("Unknown engine")
+}
+
+// IsEngineAvailable checks that the given engine is available with
+// the current `wasmer-go` built.
+//
+//   IsEngineAvailable(JIT)
+func IsEngineAvailable(engine EngineKind) bool {
+	return bool(C.wasmer_is_engine_available(C.wasmer_engine_t(engine)))
+}
+
 // Config holds the compiler and the Engine used by the Store.
 type Config struct {
 	_inner *C.wasm_config_t
@@ -27,9 +101,12 @@ func (self *Config) inner() *C.wasm_config_t {
 //
 //   config := NewConfig()
 //   config.UseJITEngine()
-//
 func (self *Config) UseJITEngine() *Config {
-	C.wasm_config_set_engine(self.inner(), C.JIT)
+	if !IsEngineAvailable(JIT) {
+		panic("This `wasmer-go` version doesn't include the JIT engine")
+	}
+
+	C.wasm_config_set_engine(self.inner(), C.wasmer_engine_t(JIT))
 
 	return self
 }
@@ -38,9 +115,12 @@ func (self *Config) UseJITEngine() *Config {
 //
 //   config := NewConfig()
 //   config.UseNativeEngine()
-//
 func (self *Config) UseNativeEngine() *Config {
-	C.wasm_config_set_engine(self.inner(), C.NATIVE)
+	if !IsEngineAvailable(NATIVE) {
+		panic("This `wasmer-go` version doesn't include the NATIVE engine")
+	}
+
+	C.wasm_config_set_engine(self.inner(), C.wasmer_engine_t(NATIVE))
 
 	return self
 }
@@ -49,9 +129,12 @@ func (self *Config) UseNativeEngine() *Config {
 //
 //   config := NewConfig()
 //   config.UseCraneliftCompiler()
-//
 func (self *Config) UseCraneliftCompiler() *Config {
-	C.wasm_config_set_engine(self.inner(), C.CRANELIFT)
+	if !IsCompilerAvailable(CRANELIFT) {
+		panic("This `wasmer-go` version doesn't include the Cranelift compiler")
+	}
+
+	C.wasm_config_set_compiler(self.inner(), C.wasmer_compiler_t(CRANELIFT))
 
 	return self
 }
@@ -60,9 +143,12 @@ func (self *Config) UseCraneliftCompiler() *Config {
 //
 //   config := NewConfig()
 //   config.UseLLVMCompiler()
-//
 func (self *Config) UseLLVMCompiler() *Config {
-	C.wasm_config_set_engine(self.inner(), C.LLVM)
+	if !IsCompilerAvailable(LLVM) {
+		panic("This `wasmer-go` version doesn't include the LLVM compiler")
+	}
+
+	C.wasm_config_set_compiler(self.inner(), C.wasmer_compiler_t(LLVM))
 
 	return self
 }
@@ -72,9 +158,12 @@ func (self *Config) UseLLVMCompiler() *Config {
 //
 //   config := NewConfig()
 //   config.UseSinglepassCompiler()
-//
 func (self *Config) UseSinglepassCompiler() *Config {
-	C.wasm_config_set_engine(self.inner(), C.SINGLEPASS)
+	if !IsCompilerAvailable(SINGLEPASS) {
+		panic("This `wasmer-go` version doesn't include the Singlepass compiler")
+	}
+
+	C.wasm_config_set_compiler(self.inner(), C.wasmer_compiler_t(SINGLEPASS))
 
 	return self
 }
