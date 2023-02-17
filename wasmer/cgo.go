62d85068ec47fd8c5febd56b81fb4b5e89e55c15
Oleksandr Brezhniev <oleksandr.brezhniev@gmail.com>
2023-02-17 18:33:29 +0200

    Enable linking with wasmer lib on linux-aarch64 platform

--- wasmer/cgo.go
+++ wasmer/cgo.go
@@ -7,7 +7,7 @@ package wasmer
 // #cgo LDFLAGS: -lwasmer
 //
 // #cgo linux,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/linux-amd64 -L${SRCDIR}/packaged/lib/linux-amd64
-// //#cgo linux,arm64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/linux-aarch64 -L${SRCDIR}/packaged/lib/linux-aarch64
+// #cgo linux,arm64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/linux-aarch64 -L${SRCDIR}/packaged/lib/linux-aarch64
 // #cgo darwin,amd64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/darwin-amd64 -L${SRCDIR}/packaged/lib/darwin-amd64
 // #cgo darwin,arm64 LDFLAGS: -Wl,-rpath,${SRCDIR}/packaged/lib/darwin-aarch64 -L${SRCDIR}/packaged/lib/darwin-aarch64
 //
